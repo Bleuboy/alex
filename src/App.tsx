cbdc5dec6446700f3512cecaf3f5afadf51cbf8b
cdans <ch.dansard@gmail.com>
2024-01-14 14:57:45 +0100

    Adjust background image

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,27 @@
 import { Route, Routes } from 'react-router-dom';
 
-import Home from './pages/Home';
 import AddDocumentPage from './pages/AddDocumentPage';
 import Facts from './pages/Facts';
+import Home from './pages/Home';
+
+const AbsoluteBackground = () => {
+  return <div className="background w-full h-full fixed"></div>;
+};
+
+// add the background to the app
 
 const App = () => {
   return (
-    <main className="bg-background w-full h-screen flex flex-col justify-center items-center overflow-hidden">
-      <Routes>
-        <Route path="" element={<Home />}></Route>
-        <Route path="/upload" element={<AddDocumentPage />}></Route>
-        <Route path="/facts" element={<Facts />}></Route>
-      </Routes>
-    </main>
+    <div className="relative w-full h-screen">
+      <AbsoluteBackground />
+      <main className="w-full h-screen flex flex-col justify-center items-center">
+        <Routes>
+          <Route path="" element={<Home />}></Route>
+          <Route path="/upload" element={<AddDocumentPage />}></Route>
+          <Route path="/facts" element={<Facts />}></Route>
+        </Routes>
+      </main>
+    </div>
   );
 };
 
