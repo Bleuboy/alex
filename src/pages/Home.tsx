104f38a73f497d05010a36ef711000da72709722
cdans <ch.dansard@gmail.com>
2024-01-14 14:57:27 +0100

    Add flexible grid component and adapt sections

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,21 @@ import { Link } from 'react-router-dom';
 
 import Button from '../components/Button';
 
+// GridView which takes a list of strings and renders them in a flexible grid
+type GridViewProps = {
+  items: string[];
+};
+
+const GridView = ({ items }: GridViewProps) => {
+  return (
+    <div className="grid grid-cols-3 gap-4">
+      {items.map((item) => (
+        <div className="bg-gray-200 p-4">{item}</div>
+      ))}
+    </div>
+  );
+};
+
 const HeaderSection = () => {
   return (
     <div>
@@ -23,38 +38,43 @@ const HeaderSection = () => {
   );
 };
 
+const FEATURES = [
+  'AI-Enhanced Factual Analysis: Consistent and Reliable Insights',
+  'Drag & Drop Application powered by AI: Enhanced Decision Accuracy',
+  'AI-Driven Evidence Check and Compilation: Seamless Workflow Integration',
+  'Improved Focus on Complex Cases: Automated Case Registration',
+];
+
 const FeaturesSection = () => {
   return (
     <div>
       <h2 className="text-3xl font-semibold text-center">Our Features</h2>
-      <ul className="list-disc list-inside">
-        <li>AI-Enhanced Factual Analysis: Consistent and Reliable Insights</li>
-        <li>
-          Drag & Drop Application powered by AI: Enhanced Decision Accuracy
-        </li>
-        <li>
-          AI-Driven Evidence Check and Compilation: Seamless Workflow
-          Integration
-        </li>
-        <li>Improved Focus on Complex Cases: Automated Case Registration</li>
-      </ul>
+      <GridView items={FEATURES} />
     </div>
   );
 };
+
+const BENEFITS = [
+  'Consistent and Reliable Insights',
+  'Enhanced Decision Accuracy',
+  'Seamless Workflow Integration',
+  'Improved Focus on Complex Cases',
+];
 
 const BenefitsSection = () => {
   return (
     <div>
       <h2 className="text-3xl font-semibold text-center">Your Benefits</h2>
-      <ul className="list-disc list-inside">
-        <li>Consistent and Reliable Insights</li>
-        <li>Enhanced Decision Accuracy</li>
-        <li>Seamless Workflow Integration</li>
-        <li>Improved Focus on Complex Cases</li>
-      </ul>
+      <GridView items={BENEFITS} />
     </div>
   );
 };
+
+const USPS = [
+  'Familiar interface that structures information according to judicial best practices',
+  'Judicially tailored LLM for nuanced brief analysis with transparent results',
+  'Encrypted data handling with privacy-centric-processing',
+];
 
 const USPsSection = () => {
   return (
@@ -62,20 +82,17 @@ const USPsSection = () => {
       <h2 className="text-3xl font-semibold text-center">
         Our Unique Selling Propositions
       </h2>
-      <ul className="list-disc list-inside">
-        <li>
-          Familiar interface that structures information according to judicial
-          best practices
-        </li>
-        <li>
-          Judicially tailored LLM for nuanced brief analysis with transparent
-          results
-        </li>
-        <li>Encrypted data handling with privacy-centric-processing</li>
-      </ul>
+      <GridView items={USPS} />
     </div>
   );
 };
+
+const COMPETITORS = [
+  'Input Modules Justiz by paradatec',
+  'Pen & Paper',
+  'Codefy',
+  'University Regensburg',
+];
 
 const CompetitorsSection = () => {
   return (
@@ -85,19 +102,17 @@ const CompetitorsSection = () => {
         ALex outperforms the competition in terms of automation and
         user-friendliness.
       </p>
-      <ul className="list-disc list-inside">
-        <li>Input Modules Justiz by paradatec</li>
-        <li>Pen & Paper</li>
-        <li>Codefy</li>
-        <li>University Regensburg</li>
-      </ul>
+      <GridView items={COMPETITORS} />
     </div>
   );
 };
 
 const Home = () => {
   return (
-    <div>
+    <div className="w-full h-full flex-grow flex flex-col gap-4">
+      <header className="flex flex-row items-center">
+        <div></div>
+      </header>
       <HeaderSection />
       <FeaturesSection />
       <BenefitsSection />
