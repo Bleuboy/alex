d0be7d30e720ab1ed93434cad82623aaf5d73c00
cdans <ch.dansard@gmail.com>
2024-01-15 10:49:51 +0100

    Add dropdown menu to Navbar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { AnchorHTMLAttributes } from 'react';
+import { AnchorHTMLAttributes, useEffect, useRef, useState } from 'react';
 
 import { Link } from 'react-router-dom';
 
@@ -37,7 +37,7 @@ type InfoSectionProps = {
 
 const InfoSection = ({ id, title, description, items }: InfoSectionProps) => {
   return (
-    <div id={id} className="flex flex-col justify-center min-h-screen">
+    <div id={id} className="flex flex-col justify-center min-h-screen pt-24">
       <h2 className="text-3xl font-semibold text-center pb-2">{title}</h2>
       <p className="text-center pb-16">{description}</p>
       <GridView items={items} />
@@ -61,19 +61,104 @@ const AlexLink = ({ className, children, ...props }: AlexLinkProps) => {
   );
 };
 
-const NavbarSection = () => {
+const LINKS = [
+  {
+    href: '#use-case',
+    title: 'Use Case',
+  },
+  {
+    href: '#our-tool',
+    title: 'Our Tool',
+  },
+  {
+    href: '#technology',
+    title: 'Technology',
+  },
+  {
+    href: '#comparison',
+    title: 'Comparison',
+  },
+  {
+    href: '#data-privacy',
+    title: 'Data Privacy',
+  },
+];
+
+const NavigationBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const node = useRef(null);
+
+  const handleClick = (event: MouseEvent) => {
+    setIsOpen((state) => !state);
+
+    if (event.target instanceof HTMLElement) {
+      if (event.target.tagName === 'A') {
+        const href = event.target.getAttribute('href');
+
+        if (href) {
+          window.location.href = href;
+        }
+      }
+    }
+  };
+
+  useEffect(() => {
+    // Add the event listener when the dropdown is open
+    if (isOpen) {
+      document.addEventListener('mousedown', handleClick);
+    } else {
+      // Remove the event listener when the dropdown is closed
+      document.removeEventListener('mousedown', handleClick);
+    }
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      document.removeEventListener('mousedown', handleClick);
+    };
+  }, [isOpen]);
+
   return (
-    <div className=" px-4 py-4 flex justify-between items-center sticky top-0 bg-white bg-opacity-80 backdrop-blur-md z-10">
+    <div
+      className="px-4 py-4 flex justify-between items-center sticky top-0 bg-white bg-opacity-80 backdrop-blur-md z-10"
+      ref={node}
+    >
       <div className="flex items-center gap-4">
         <a href="#">
           <img src="/alex-logo.png" className="w-12 h-12" />
         </a>
-        {/* Add links to every section of this page, with automatic scrolling */}
-        <AlexLink href="#use-case">Use Case</AlexLink>
-        <AlexLink href="#our-tool">Our Tool</AlexLink>
-        <AlexLink href="#technology">Technology</AlexLink>
-        <AlexLink href="#comparison">Comparison</AlexLink>
-        <AlexLink href="#data-privacy">Data Privacy</AlexLink>
+        <div className="hidden md:flex gap-4">
+          {LINKS.map(({ href, title }) => (
+            <AlexLink href={href}>{title}</AlexLink>
+          ))}
+        </div>
+        <div className="md:hidden">
+          <button
+            className="font-semibold"
+            onClick={() => setIsOpen((state) => !state)}
+          >
+            Menu
+          </button>
+          {isOpen && (
+            <div className="absolute left-0 mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+              <div
+                className="py-1"
+                role="menu"
+                aria-orientation="vertical"
+                aria-labelledby="options-menu"
+              >
+                {LINKS.map(({ href, title }) => (
+                  <AlexLink
+                    href={href}
+                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                    role="menuitem"
+                  >
+                    {title}
+                  </AlexLink>
+                ))}
+              </div>
+            </div>
+          )}
+        </div>
       </div>
       <div>
         <Link to="/upload">
@@ -196,7 +281,7 @@ const CompetitorsSection = () => {
 const Home = () => {
   return (
     <div className="mx-auto max-w-7xl">
-      <NavbarSection />
+      <NavigationBar />
       <div className="px-4 py-4 mx-auto flex flex-col gap-16">
         <HeaderSection />
         <UseCaseSection />
