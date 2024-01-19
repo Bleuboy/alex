86c110b4fd7fbadd71065dbdc76515fe3b57f59c
marwanabdalla1 <86526673+marwanabdalla1@users.noreply.github.com>
2024-01-20 00:01:49 +0100

    Add PDFtoTextConversion

--- src/pages/Facts.tsx
+++ src/pages/Facts.tsx
@@ -1,12 +1,50 @@
-import { useState } from 'react';
-
+import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getDocument } from 'pdfjs-dist';
+import {
+  CourtFile,
+  RootState,
+} from '../types';
 import Button from '../components/Button';
 import CrossExam from '../components/CrossExam';
 import { Aspect, Filter, Testimony } from '../types';
+import { TextItem } from 'pdfjs-dist/types/src/display/api';
 
 const Facts = () => {
   const [filter, setFilter] = useState<Filter>('aspect');
 
+  const documents = useSelector((state: RootState) => state.documents);
+  
+  const dispatch = useDispatch();
+
+  const courtFiles = documents.courtFiles;
+
+
+// Trying to get the text from the PDFs
+
+  useEffect(() => {
+    const convertPDFToText = async (file: File) => {
+      const pdf = await getDocument({ url: URL.createObjectURL(file) }).promise;
+      let text = '';
+    
+      for (let i = 1; i <= pdf.numPages; i++) {
+        const page = await pdf.getPage(i);
+        const content = await page.getTextContent();
+        text += content.items
+          .filter((item): item is TextItem => 'str' in item)
+          .map(item => item.str)
+          .join(' ');
+      }
+    
+      console.log(text);
+    };
+  
+    courtFiles.forEach((courtFile) => {
+      if (courtFile.document) {
+        convertPDFToText(courtFile.document);
+      }
+    });
+  }, [courtFiles]);
   const testimonies: Testimony[] = [
     {
       party: 'Claimant',
